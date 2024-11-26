25f2f4c38d391613f046fec53702ac7d00782360
keerthisaladi11 <keerthisaladi12@gmail.com>
2024-11-26 13:47:38 +0530

    picker

---
+++ picker.js
@@ -0,0 +1,179 @@
+var a = document.querySelector("h2");
+var color1 = ()=>
+{
+    a.style.color = "red";
+}
+var color2 = ()=>
+{
+    a.style.color = "yellow";
+}
+var color3 = ()=>
+{
+    a.style.color = "green";
+}
+var color4 = ()=>
+{
+    a.style.color = "blue";
+}
+var color5 = ()=>
+{
+    a.style.color = "aqua";
+}
+var color6 = ()=>
+{
+    a.style.color = "blueviolet";
+}
+var color7 = ()=>
+{
+    a.style.color = "brown";
+}
+var color8 = ()=>
+{
+    a.style.color = "burlywood";
+}
+var color9 = ()=>
+{
+    a.style.color = "cadetblue";
+}
+var color10 = ()=>
+{
+    a.style.color = "chartreuse";
+}
+var color11 = ()=>
+{
+    a.style.color = "chocolate";
+}
+var color12 = ()=>
+{
+    a.style.color = "cornflowerblue";
+}
+var color13 = ()=>
+{
+    a.style.color = "cyan";
+}
+var color14 = ()=>
+{
+    a.style.color = "darkcyan";
+}
+var color15 = ()=>
+{
+    a.style.color = "darkgreen";
+}
+
+var bgcolor1 = ()=>
+{
+    a.style.backgroundColor="red";
+}
+var bgcolor2 = ()=>
+{
+    a.style.backgroundColor = "yellow";
+}
+var bgcolor3 = ()=>
+{
+    a.style.backgroundColor = "green";
+}
+var bgcolor4 = ()=>
+{
+    a.style.backgroundColor = "blue";
+}
+var bgcolor5 = ()=>
+{
+    a.style.backgroundColor = "aqua";
+}
+var bgcolor6 = ()=>
+{
+    a.style.backgroundColor = "blueviolet";
+}
+var bgcolor7 = ()=>
+{
+    a.style.backgroundColor = "brown";
+}
+var bgcolor8 = ()=>
+{
+    a.style.backgroundColor = "burlywood";
+}
+var bgcolor9 = ()=>
+{
+    a.style.backgroundColor = "cadetblue";
+}
+var bgcolor10 = ()=>
+{
+    a.style.backgroundColor = "chartreuse";
+}
+var bgcolor11 = ()=>
+{
+    a.style.backgroundColor = "chocolate";
+}
+var bgcolor12 = ()=>
+{
+    a.style.backgroundColor = "cornflowerblue";
+}
+var bgcolor13 = ()=>
+{
+    a.style.backgroundColor = "cyan";
+}
+var bgcolor14 = ()=>
+{
+    a.style.backgroundColor = "darkcyan";
+}
+var bgcolor15 = ()=>
+{
+    a.style.backgroundColor = "darkgreen";
+}
+
+var font1= ()=>
+{
+    a.style.fontFamily="Times New Roman"
+}
+var font2= ()=>
+{
+    a.style.fontFamily="Roboto"
+}
+var font3= ()=>
+{
+    a.style.fontFamily="Courier New"
+}
+var font4= ()=>
+{
+    a.style.fontFamily="cursive"
+}
+var font5= ()=>
+{
+    a.style.fontFamily="Verdana"
+}
+var font6= ()=>
+{
+    a.style.fontFamily="Impact"
+}
+var font7= ()=>
+{
+    a.style.fontFamily="Arial"
+}
+var font8= ()=>
+{
+    a.style.fontFamily="Georgia"
+}
+var font9= ()=>
+{
+    a.style.fontFamily="serif";
+}
+var font10= ()=>
+{
+    a.style.fontFamily="sans-serif";
+}
+var font11= ()=>
+{
+    a.style.fontFamily="Georgia"
+}
+var font12= ()=>
+{
+    a.style.fontFamily="Courier";;
+}
+var font13= ()=>
+{
+    a.style.fontFamily="Helvetica";
+}
+var font14= ()=>
+{
+    a.style.fontFamily="Times";
+}
